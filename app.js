08a1507e6498d7e28d6ff5add59bc79daa0dc18b
Torsten Bergh Moss <torsten@Torstens-MacBook-Pro.local>
2019-03-18 17:21:28 -0300

    Added samplecode from sparql wikidata site

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 var express = require('express');
+const fetch = require("node-fetch");
 
 var port = process.env.PORT || 3000;
 var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
-});
-app.listen(port, function () {
- console.log(`Example app listening on port`, port);
-});
+
+var wikiData = "Heyhey";
+
+class SPARQLQueryDispatcher {
+	constructor( endpoint ) {
+		this.endpoint = endpoint;
+	}
+
+	query( sparqlQuery ) {
+		const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+		const headers = { 'Accept': 'application/sparql-results+json' };
+
+		return fetch( fullUrl, { headers } ).then( body => body.json() );
+	}
+}
+
+const endpointUrl = 'https://query.wikidata.org/sparql';
+const sparqlQuery = `#Cats, with pictures
+#added before 2016-10
+
+#defaultView:ImageGrid
+SELECT ?item ?itemLabel ?pic
+WHERE
+{
+?item wdt:P31 wd:Q146 .
+?item wdt:P18 ?pic
+SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+}`;
+
+const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
+queryDispatcher.query( sparqlQuery ).then( wikiData => {
+    console.log("got here");
+    console.log(wikiData.results.bindings);
+    app.get('/', function (req, res) {
+        res.send(JSON.stringify(wikiData.results.bindings));
+       });
+    app.listen(port, function () {
+    console.log(`Example app listening on port`, port);
+    });
+}) 
+.catch( console.log );
+
+
+
+
