5dad374cfe87caa6c51d099439cdd7b41c8f42b4
Torsten Bergh Moss <torsten@Torstens-MacBook-Pro.local>
2019-05-27 14:30:18 -0400

    Add access-control-allow-origin

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Define SPARQL endpoint, TODO: Make dynamic
 const endpointUrl = 'https://query.wikidata.org/sparql';
