7dd51fea44703d3fac62ca8273e80e51688626de
marasva <mariesva@stud.ntnu.no>
2019-07-01 21:17:20 -0400

    Updated file and configurations

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN yarn
 COPY . .
-EXPOSE 3000
+ENV SERVER_PORT 3020
+EXPOSE 3020
 CMD [ "yarn", "start" ]
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Imports
-var { QueryFactory, QueryRepository  } = require("./model/model");
+// Imports
+var { QueryFactory, QueryRepository } = require("./model/model");
 var express = require('express');
 var bodyParser = require("body-parser");
 const fetch = require("node-fetch");
@@ -10,11 +11,11 @@ var port = process.env.PORT || 3020;
 var app = express();
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 // Define SPARQL endpoint, TODO: Make dynamic
 const endpointUrl = 'https://query.wikidata.org/sparql';
@@ -23,12 +24,12 @@ const endpointUrl = 'https://query.wikidata.org/sparql';
 // runQuery : Takes sparqlQueryObject as input and populates sparqlQueryObject.queryResult
 runQuery = sparqlQueryObject => {
     endpoint = endpointUrl;
-    const fullUrl = endpoint + '?query=' +  encodeURIComponent(sparqlQueryObject.queryString);
+    const fullUrl = endpoint + '?query=' + encodeURIComponent(sparqlQueryObject.queryString);
     const headers = { 'Accept': 'application/sparql-results+json' };
 
-    return fetch( fullUrl, { headers } )
-        .then( body => body.json())
-        .then( result => {
+    return fetch(fullUrl, { headers })
+        .then(body => body.json())
+        .then(result => {
             sparqlQueryObject.setResult(result);
             return sparqlQueryObject
         });
@@ -43,7 +44,7 @@ var queryFactory = new QueryFactory();
 app.post("/runQuery", (request, response) => {
     let queryObject = queryFactory.createQueryObject(request.body.query, null);
     runQuery(queryObject)
-    .then(queryObject => response.send(queryObject.queryResult.results.bindings))
+        .then(queryObject => response.send(queryObject.queryResult.results.bindings))
 })
 
 app.get("/", (request, response) => {
@@ -54,3 +55,59 @@ app.get("/", (request, response) => {
 app.listen(port, function () {
     console.log(`SPARQL Query microservice listening on port`, port);
 });
+// var { QueryFactory, QueryRepository  } = require("./model/model");
+// var express = require('express');
+// var bodyParser = require("body-parser");
+// const fetch = require("node-fetch");
+
+
+// // Config
+// var port = process.env.PORT || 3020;
+// var app = express();
+// app.use(express.json())
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+// // Define SPARQL endpoint, TODO: Make dynamic
+// const endpointUrl = 'https://query.wikidata.org/sparql';
+
+
+// // runQuery : Takes sparqlQueryObject as input and populates sparqlQueryObject.queryResult
+// runQuery = sparqlQueryObject => {
+//     endpoint = endpointUrl;
+//     const fullUrl = endpoint + '?' +  encodeURIComponent(sparqlQueryObject.queryString);
+//     const headers = { 'Accept': 'application/sparql-results+json' };
+
+//     return fetch( fullUrl, { headers } )
+//         .then( body => body.json())
+//         .then( result => {
+//             sparqlQueryObject.setResult(result);
+//             return sparqlQueryObject
+//         });
+// }
+
+
+// // Initialize query factory
+// var queryFactory = new QueryFactory();
+
+
+// // REST API: Handle incoming POST request
+// app.post("/runQuery", (request, response) => {
+//     console.log("fetching response")
+//     let queryObject = queryFactory.createQueryObject(request.body.query, null);
+//     runQuery(queryObject)
+//     .then(queryObject => response.send(queryObject.queryResult.results.bindings))
+// })
+
+// app.get("/", (request, response) => {
+//     response.send("SPARQL-service is running");
+// })
+
+// // Start listening
+// app.listen(port, function () {
+//     console.log(`SPARQL Query microservice listening on port`, port);
+// });
